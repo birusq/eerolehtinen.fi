c6741f504e6155631a49e6412ee73735a6536321
Eero Lehtinen <eero@eerolehtinen.fi>
2021-01-13 18:40:08 +0200

    Test static props revalidation

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,9 +1,24 @@
-const Test = () => {
+import getRandNum from '../utils/rand-num-api'
+
+const Test = ({ num }: { num: number }) => {
 	return (
 		<p>
-			
+			{num}
 		</p>
 	)
+}
+
+export async function getStaticProps() {
+	const num = await getRandNum()
+
+	console.log(num)
+
+	return {
+		props: {
+			num,
+		},
+		revalidate: 1,
+	}
 }
 
 export default Test
--- utils/rand-num-api.ts
+++ utils/rand-num-api.ts
@@ -1,7 +1,9 @@
 const getRandNum = async () => {
-	const res = await fetch("https://www.random.org/integers/\
-		?num=1&min=1&max=1000000&col=1&base=10&format=plain&rnd=new")
-	return res
+	const res = await fetch("https://www.random.org/integers/" +
+		"?num=1&min=1&max=1000000&col=1&base=10&format=plain&rnd=new")
+
+	const text = await res.text()
+	return parseInt(text)
 }
 
 export default getRandNum
